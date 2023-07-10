b06a4d6a359319f3faf69a96825f256e4d5c0705
doctorgde1 <rakov5377357536277hfhsghjsgh@gmail.com>
2023-07-10 14:13:14 +0300

    refactor: added react router

---
+++ src/components/icons/Cross.tsx
@@ -0,0 +1,19 @@
+export type ICross = React.SVGAttributes<SVGElement>;
+const Cross: React.FC<ICross> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M18 6L6 18M6 6l12 12" />
+    </svg>
+  );
+};
+
+export default Cross;
